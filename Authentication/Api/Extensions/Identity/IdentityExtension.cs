9607c7f63c8df88947fa9b3543e492ad34f7c826
Dzenan Dzafic <dzeno.dzafic@windowslive.com>
2023-03-03 18:52:12 +0100

    Removing parts that are breaking the app

--- Authentication/Api/Extensions/Identity/IdentityExtension.cs
+++ Authentication/Api/Extensions/Identity/IdentityExtension.cs
@@ -53,43 +53,7 @@ namespace Api.Extensions.Identity
 
             var servicesProvider = services.BuildServiceProvider();
             
-            InitializeDatabase(servicesProvider);
-
             return services;
-        }
-
-        private static async void InitializeDatabase(ServiceProvider provider)
-        {
-            using var serviceScope = provider.GetService<IServiceScopeFactory>().CreateScope();
-            serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
-
-            var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
-
-            context.Database.Migrate();
-
-            if (!context.Clients.Any())
-            {
-                foreach (var client in Configuration.Clients.ToList())
-                    await context.Clients.AddAsync(client.ToEntity());
-
-                await context.SaveChangesAsync();
-            }
-
-            if (!context.IdentityResources.Any())
-            {
-                foreach (var resource in Configuration.IdentityResources)
-                    await context.IdentityResources.AddAsync(resource.ToEntity());
-
-                await context.SaveChangesAsync();
-            }
-
-            if (!context.ApiScopes.Any())
-            {
-                foreach (var scope in Configuration.ApiScopes)
-                    await context.ApiScopes.AddAsync(scope.ToEntity());
-
-                await context.SaveChangesAsync();
-            }
         }
     }
 }
